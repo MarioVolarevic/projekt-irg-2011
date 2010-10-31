28770955e46b9da4ac937dcb7c57db0fe3f5a1db
mario.volarevic <mario.volarevic@86cc4e84-eea2-baa8-f4a2-eb7baa844040>
2010-10-31 21:11:06 +0000

    - dodao u source model korkove formule i kod za tipkovnicu koji je napisao mrki - prema korkovom prijedlogu izbacio intelisense bazu i ono sto nastane prilikom kompajliranja, .suo file neka ostane - dodao novu funkcionalnost (kada se gleda samo markerA onda je na njemu model formule, kad se stavi blizu markeraB onda se postavi formula na cestu - zarotirao sam korkov model i centrirao ga, da odgovara kodu, jos ce ga mozda i trebat skalirat, al to kad ustvrdimo dimenzije koje ce se koristit

--- trunk/Source/FERmula_1/fermula.cpp
+++ trunk/Source/FERmula_1/fermula.cpp
@@ -22,8 +22,59 @@
 #include <osgDB/FileUtils>
 #include <osgDB/ReadFile>
 
-osg::ref_ptr<osg::MatrixTransform> pos_ces = new osg::MatrixTransform();
+osg::ref_ptr<osg::MatrixTransform> tran_fer = new osg::MatrixTransform();
 
+#pragma region Proximity Callback
+
+class MarkerProximityUpdateCallback : public osg::NodeCallback {
+
+private:
+	osg::MatrixTransform* mtA;
+	osg::MatrixTransform* mtB;
+
+	osg::Switch* mSwitchA;
+
+	float mThreshold;
+
+public:
+
+	MarkerProximityUpdateCallback(osg::MatrixTransform* mA, osg::MatrixTransform* mB, osg::Switch* switchA, float threshold) : 
+	  osg::NodeCallback(), 
+		  mtA(mA), mtB(mB),
+		  mSwitchA(switchA), mThreshold(threshold) { }
+
+
+	  virtual void operator()(osg::Node* node, osg::NodeVisitor* nv) {
+
+		  /** CALCULATE INTER-MARKER PROXIMITY:
+		  Here we obtain the current position of each marker, and the
+		  distance between them by examining
+		  the translation components of their parent transformation 
+		  matrices **/
+		  osg::Vec3 posA = mtA->getMatrix().getTrans();
+		  osg::Vec3 posB = mtB->getMatrix().getTrans();
+		  osg::Vec3 offset = posA - posB;
+		  float distance = offset.length();
+		  //scene->setUpdateCallback(new MarkerProximityUpdateCallback(mtA, mtB,switchA.get(),switchB.get(),200.0f)); 
+
+		  /** LOAD APPROPRIATE MODELS:
+		  Here we use each marker's OSG Switch node to swap between
+		  models, depending on the inter-marker distance we have just 
+		  calculated. **/
+		  if (distance <= mThreshold) {
+			  if (mSwitchA->getNumChildren() > 1) mSwitchA->setSingleChildOn(1);
+		  } else {
+			  if (mSwitchA->getNumChildren() > 0) mSwitchA->setSingleChildOn(0);
+		  }
+
+
+		  traverse(node,nv);
+
+	  }
+};
+#pragma endregion
+
+#pragma region Keyboard Handler
 
 class MyKeyboardEventHandler : public osgGA::GUIEventHandler {
 
@@ -56,7 +107,37 @@ public:
 				{
 				case osgGA::GUIEventAdapter::KEY_Up: // Move forward 5mm
 					//driveCar->preMult(osg::Matrix::translate(0, -5, 0));
-					pos_ces->preMult(osg::Matrix::translate(0,-5,0));
+					tran_fer->preMult(osg::Matrix::translate(0,-5,0));
+					return true;
+
+				case osgGA::GUIEventAdapter::KEY_Down:
+					tran_fer->preMult(osg::Matrix::translate(0,5,0));
+					return true;
+
+				case osgGA::GUIEventAdapter::KEY_Left:
+					tran_fer->preMult(osg::Matrix::rotate(osg::DegreesToRadians(10.0f), osg::Z_AXIS));
+					tran_fer->preMult(osg::Matrix::translate(0,-5,0));
+					return true;
+
+				case osgGA::GUIEventAdapter::KEY_Right:
+					tran_fer->preMult(osg::Matrix::rotate(osg::DegreesToRadians(-10.0f), osg::Z_AXIS));
+					tran_fer->preMult(osg::Matrix::translate(0,-5,0));
+					return true;
+
+				case osgGA::GUIEventAdapter::KEY_Page_Down:
+					tran_fer->preMult(osg::Matrix::rotate(osg::DegreesToRadians(10.0f), osg::X_AXIS));   //pokret prema dolje pod kutom
+					tran_fer->preMult(osg::Matrix::translate(0,-5,0));
+					return true;
+
+				case osgGA::GUIEventAdapter::KEY_Page_Up:
+					tran_fer->preMult(osg::Matrix::rotate(osg::DegreesToRadians(-10.0f), osg::X_AXIS));  //pokret prema gore pod kutom
+					tran_fer->preMult(osg::Matrix::translate(0,-5,0));
+					return true;
+
+				case osgGA::GUIEventAdapter::KEY_BackSpace:  //Reset transformation
+					tran_fer->setMatrix(osg::Matrix::identity());
+					tran_fer->preMult(osg::Matrix::translate(osg::Vec3(0,0,25)));
+					tran_fer->preMult(osg::Matrix::scale(osg::Vec3(2,2,2)));
 					return true;
 
 				case ' ': // Reset the transformation
@@ -64,15 +145,15 @@ public:
 					//osg::Matrix *m;
 					//cesta->computeWorldToLocalMatrix(*m,nv);
 					//osg::notify(osg::WARN)<<"matrica: "<<*m<<std::endl;
-					//pos_ces->setMatrix(*m);
+					//tran_fer->setMatrix(*m);
 					//osg::Vec3 v;
 					//arTransformA->computeLocalToWorldMatrix(*m,nv);
 					//osg::notify(osg::WARN)<<"matrica m: "<<*m<<std::endl << m<<std::endl;
 					//osg::notify(osg::WARN)<<"posit arTransformA: "<<arTransformA->getPosition()<<std::endl;
-					//pos_ces->setPosition(arTransformA->getPosition());
-					//pos_ces->setScale(osg::Vec3(2,2,2));
-					//osg::notify(osg::WARN)<<"posit pos_ces: "<<pos_ces->getPosition()<<std::endl;
-					//pos_ces->setAttitude(arTransformA->getMatrix
+					//tran_fer->setPosition(arTransformA->getPosition());
+					//tran_fer->setScale(osg::Vec3(2,2,2));
+					//osg::notify(osg::WARN)<<"posit tran_fer: "<<tran_fer->getPosition()<<std::endl;
+					//tran_fer->setAttitude(arTransformA->getMatrix
 					return true;
 				}
 
@@ -84,7 +165,9 @@ public:
 		}
 	}
 };
+#pragma endregion
 
+#pragma region Custom Visibility Callback
 class MyMarkerVisibilityCallback : public osgART::MarkerVisibilityCallback
 {
 public:
@@ -102,6 +185,8 @@ public:
 };
 
 
+#pragma endregion //zasad se ne koristi
+
 osg::Group* createImageBackground(osg::Image* video) {
 	osgART::VideoLayer* _layer = new osgART::VideoLayer();
 	_layer->setSize(*video);
@@ -110,6 +195,8 @@ osg::Group* createImageBackground(osg::Image* video) {
 	_layer->addChild(_geode);
 	return _layer;
 }
+
+
 
 int main (int argc, char * argv[])
 {
@@ -128,13 +215,11 @@ int main (int argc, char * argv[])
 
 	// preload the video and tracker
 	int _video_id = osgART::PluginManager::instance()->load("osgart_video_artoolkit2");
-
 	int _tracker_id = osgART::PluginManager::instance()->load("osgart_tracker_artoolkit2");
 
-	// Load a video plugin.
-	osg::ref_ptr<osgART::Video> video = 
-		dynamic_cast<osgART::Video*>(osgART::PluginManager::instance()->get(_video_id));
 
+	// Load video plugin.
+	osg::ref_ptr<osgART::Video> video = dynamic_cast<osgART::Video*>(osgART::PluginManager::instance()->get(_video_id));
 	// check if an instance of the video stream could be started
 	if (!video.valid()) 
 	{   
@@ -142,26 +227,26 @@ int main (int argc, char * argv[])
 		osg::notify(osg::FATAL) << "Could not initialize video plugin!" << std::endl;
 		exit(-1);
 	}
-
 	// Open the video. This will not yet start the video stream but will
 	// get information about the format of the video which is essential
 	// for the connected tracker
 	video->open();
 
-	osg::ref_ptr<osgART::Tracker> tracker = 
-		dynamic_cast<osgART::Tracker*>(osgART::PluginManager::instance()->get(_tracker_id));
 
+	//Load tracker plugin
+	osg::ref_ptr<osgART::Tracker> tracker = dynamic_cast<osgART::Tracker*>(osgART::PluginManager::instance()->get(_tracker_id));
 	if (!tracker.valid()) 
 	{
 		// Without tracker an AR application can not work. Quit if none found.
 		osg::notify(osg::FATAL) << "Could not initialize tracker plugin!" << std::endl;
 		exit(-1);
 	}
-
 	// get the tracker calibration object
 	osg::ref_ptr<osgART::Calibration> calibration = tracker->getOrCreateCalibration();
+	// set the image source for the tracker
 
-	// load a calibration file
+
+	// load camera calibration file
 	if (!calibration->load(std::string("data/camera_para.dat"))) 
 	{
 
@@ -169,14 +254,12 @@ int main (int argc, char * argv[])
 		osg::notify(osg::FATAL) << "Non existing or incompatible calibration file" << std::endl;
 		exit(-1);
 	}
-
-	// set the image source for the tracker
 	tracker->setImage(video.get());
-	
-	osgART::TrackerCallback::addOrSet(root.get(), tracker.get());
+	//camera calibration
+	osg::ref_ptr<osg::Camera> cam = calibration->createCamera();
 
 	//markerA
-	osg::ref_ptr<osgART::Marker> markerA = tracker->addMarker("single;data/armedia.patt;80;0;0");
+	osg::ref_ptr<osgART::Marker> markerA = tracker->addMarker("single;data/patt.kanji;80;0;0");
 	if (!markerA.valid()) 
 	{
 		// Without marker an AR application can not work. Quit if none found.
@@ -186,7 +269,7 @@ int main (int argc, char * argv[])
 	markerA->setActive(true);
 
 	//markerB
-	osg::ref_ptr<osgART::Marker> markerB = tracker->addMarker("single;data/kvadratic.patt;80;0;0");
+	osg::ref_ptr<osgART::Marker> markerB = tracker->addMarker("single;data/armedia.patt;75;0;0");
 	if (!markerB.valid()) 
 	{
 		// Without marker an AR application can not work. Quit if none found.
@@ -195,46 +278,49 @@ int main (int argc, char * argv[])
 	}
 	markerB->setActive(true);
 
+	//video
+	osg::ref_ptr<osg::Group> videoBackground = createImageBackground(video.get());
+	videoBackground->getOrCreateStateSet()->setRenderBinDetails(0, "RenderBin");
+
 	//arTransformA
 	osg::ref_ptr<osg::MatrixTransform> arTransformA = new osg::MatrixTransform();
 	osgART::attachDefaultEventCallbacks(arTransformA.get(),markerA.get());
 	//osgART::addEventCallback(arTransformA.get(), new osgART::MarkerTransformCallback(markerA));
 	//osgART::addEventCallback(arTransformA.get(), new MyMarkerVisibilityCallback(markerA));
-
-	osg::ref_ptr<osg::MatrixTransform> cesta = new osg::MatrixTransform();
-	cesta->addChild(osgDB::readNodeFile("cesta_rav.3ds"));
-	arTransformA->addChild(cesta);
 	arTransformA->getOrCreateStateSet()->setRenderBinDetails(100, "RenderBin");
 
 	//arTransformB
 	osg::ref_ptr<osg::MatrixTransform> arTransformB = new osg::MatrixTransform();
 	osgART::attachDefaultEventCallbacks(arTransformB,markerB);
-	arTransformB->addChild(osgDB::readNodeFile("cesta_rav.3ds"));
+	arTransformB->addChild(osgDB::readNodeFile("data/cesta_rav.3ds"));
 	arTransformB->getOrCreateStateSet()->setRenderBinDetails(100,"RenderBin");
 
-	//osg::ref_ptr<osg::PositionAttitudeTransform> pos_ces = new osg::PositionAttitudeTransform();
-	pos_ces->addChild(osgDB::readNodeFile("cessna.osg"));
-	pos_ces->preMult(osg::Matrix::translate(osg::Vec3(0,0,25)));
-	pos_ces->preMult(osg::Matrix::scale(osg::Vec3(2,2,2)));
-	arTransformA->addChild(pos_ces);
-	//pos_ces->setScale(osg::Vec3(0.5,1,0.5));
-	//pos_ces->setUpdateCallback(osg::AnimationPathCallback( setPosition(arTransformA->getMatrix().getTrans());
-	//pos_ces->addChild(osgDB::readNodeFile("cessna.osg"));
-	//pos_ces->getOrCreateStateSet()->setRenderBinDetails(20,"RenderBin");	
-	//root->addChild(pos_ces);
+	//grupa model i cesta
+	osg::ref_ptr<osg::Group> mod_ces= new osg::Group();
+	mod_ces->addChild(tran_fer);
+	mod_ces->addChild(osgDB::readNodeFile("data/cesta_rav.3ds"));
+	
+	//transformacije modela
+	tran_fer->addChild(osgDB::readNodeFile("data/fermula_kork.3DS"));
+	tran_fer->preMult(osg::Matrix::translate(osg::Vec3(0,0,25)));
+	tran_fer->preMult(osg::Matrix::scale(osg::Vec3(2,2,2)));
 
-	osg::ref_ptr<osg::Group> videoBackground = createImageBackground(video.get());
-	videoBackground->getOrCreateStateSet()->setRenderBinDetails(0, "RenderBin");
+	//switch za prikazivanje dodatnog modela ovisno o udaljenosti markera
+	osg::ref_ptr<osg::Switch> switchA = new osg::Switch();
+	switchA->addChild(osgDB::readNodeFile("data/fermula_kork.3DS"),true);
+	switchA->addChild(mod_ces,false);
+	arTransformA->addChild(switchA.get());
+	osgART::TrackerCallback::addOrSet(root.get(), tracker.get());
 
-	osg::ref_ptr<osg::Camera> cam = calibration->createCamera();
 
-	cam->addChild(arTransformA.get());
+	cam->addChild(arTransformA);
 	cam->addChild(arTransformB);
 	cam->addChild(videoBackground.get());
 	root->addChild(cam.get());
+	root->setUpdateCallback(new MarkerProximityUpdateCallback(arTransformA, arTransformB,switchA.get(),250));
 
-video->start();
-int r = viewer.run();   
-video->close();
-return r;
+	video->start();
+	int r = viewer.run();   
+	video->close();
+	return r;
 }
